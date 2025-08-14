b74e000d66760c244c2e14abfc3f0ae83b9e0cef
8888 <a8888@8888deMacBook-Pro.local>
2025-08-14 15:10:41 +0800

    x

--- docker_build.sh
+++ docker_build.sh
@@ -4,8 +4,8 @@ set -e
 
 # IMAGE_NAME='inf-repo-registry.cn-wulanchabu.cr.aliyuncs.com/infly/knowledge-assistant'
 # IMAGE_TAG=$(git log --pretty=format:"%h" -n 1)
-IMAGE_NAME='guanxin-dev/health-ai-search-backend'
-IMAGE_TAG=1.0.2
+IMAGE_NAME='health-ai-search-backend'
+IMAGE_TAG=1.0.3
 
 docker build -t "$IMAGE_NAME:$IMAGE_TAG" -f Dockerfile .
 
